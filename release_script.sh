fcef214a5ae07b31c2d77009b3c4f68f9d38408c
Ahmed KARRAR <ahmed.karrar@valeo.com>
2026-02-01 12:42:57 +0200

    Adding Same day logic handling

--- release_script.sh
+++ release_script.sh
@@ -21,18 +21,18 @@ echo "ERROR: Tag $tag_date already exist!"
 exit 1
 fi
 
-# if [ $(git tag -l "$tag_date") ]; then
-#     echo "Tag $tag_date already exists. calculating increment..."
-#     counter=1
-#     while true; do
-#         candidate_tag="${tag_date}.${counter}"
-#         if [ ! $(git tag -l "$candidate_tag") ]; then
-#             tag_date="$candidate_tag"
-#             break
-#         fi
-#         ((counter++))
-#     done
-# fi
+if [ $(git tag -l "$tag_date") ]; then
+    echo "Tag $tag_date already exists. calculating increment..."
+    counter=1
+    while true; do
+        candidate_tag="${tag_date}.${counter}"
+        if [ ! $(git tag -l "$candidate_tag") ]; then
+            tag_date="$candidate_tag"
+            break
+        fi
+        ((counter++))
+    done
+fi
 
 echo "New tag will be: ${tag_date}"
 
